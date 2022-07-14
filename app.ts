8ef6259c04e7b0b3538bf21f598f0451f00cf7f5
perkles <mundimrbezerra@gmail.com>
2022-07-14 13:51:03 -0300

    first commit

---
+++ app.ts
@@ -0,0 +1,22 @@
+let empregado  =  {
+    codigo: 10,
+    nome: 'Pedro'
+};
+
+let empregado2: {codigo: number, nome: string} = {
+    codigo: 10,
+    nome: 'Pedro',
+}
+
+interface Empregado {
+    codigo: number,
+    nome: string
+}
+
+let empregado3: Empregado = {
+    codigo: 200,
+    nome: 'Mario'
+}
+
+
+    
